a91ab96a8e273dc3b4ec8983d44ce435e4574f29
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-05-19 17:51:37 +0600

    changing some style

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(".toggle_icon").click(function() {
   $(this).toggleClass("fas fa-eye-slash");
 });
+
+$("#testimonial").fadeIn("slow", function() {
+  $("#testimonial")
+    .delay(5000)
+    .fadeOut();
+});
